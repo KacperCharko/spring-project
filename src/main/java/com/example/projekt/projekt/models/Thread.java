8697b84aecf95ce2bae59e78766a87f84762d6f0
Kacper <39369120+KacperCharko@users.noreply.github.com>
2020-06-13 12:09:31 +0200

    Initial commit

---
+++ src/main/java/com/example/projekt/projekt/models/Thread.java
@@ -0,0 +1,65 @@
+package com.example.projekt.projekt.models;
+
+import org.apache.catalina.User;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class
+
+Thread {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long threadId;
+    private String threadName;
+    private Date creationDate;
+    private Long authorId;
+    private boolean threadState;
+
+    public boolean isThreadState() {
+        return threadState;
+    }
+
+    public void setThreadState(boolean threadState) {
+        this.threadState = threadState;
+    }
+
+    public Thread(){}
+
+    public Long getThreadId() {
+        return threadId;
+    }
+
+    public void setThreadId(Long threadId) {
+        this.threadId = threadId;
+    }
+
+    public String getThreadName() {
+        return threadName;
+    }
+
+    public void setThreadName(String threadName) {
+        this.threadName = threadName;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+}
